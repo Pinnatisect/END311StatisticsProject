258c3fba550046eb08c2338ab1eab5406610c92c
Quarre <kaankizir@gmail.com>
2020-06-08 18:52:26 +0300

    Qn.R

--- Q6.R
+++ Q6.R
@@ -1,29 +1,9 @@
-# PART 5 - Two Sample Hypothesis Testing for Total Sales in Boston and Atlanta Regions
-# We will apply t-test. Assume that each groups are APPROXIMATELY NORMALLY DISTRIBUTED WITH EQUAL VARIANCES.
+## PART6 - Regression Analysis: "4046 and "Total Bags" in Boston region
 
 library(data.table)
 
 # read the data as data table
 avo <- as.data.table(read.csv("avocado.csv"))
-
-# 1. State the hypothesis.
-# H0: there is NO significant difference between total sales (in volume) for Boston and Atlanta regions.
-# H1: there is a significant difference between total sales (in volume) for Boston and Atlanta regions.
-# Notice that this test has a two-tailed observation.
-
-atlanta <- avo[avo$region == "Atlanta", ]$Total.Volume
-boston <- avo[avo$region == "Boston", ]$Total.Volume
-# 2. Compute the test statistic.
-
-t.test(atlanta, boston, paired = F, alternative = "two.sided", var.equal = T, conf.level = 0.95)
-t_value <- (mean(atlanta)-mean(boston))/sqrt(var(atlanta)/338 + var(boston)/338)
-print(t_value)
-# 3. Interpret the results.
-# t_value = -1.217 with df = 674 and p = 0.224. p > 0.025 (two-tailed test, conf. level = 0.95 then alpha = 0.05)
-# reject null hypothesis in favor of alternative hypothesis.
-# there is a significant difference between means for Boston and Atlanta regions.
-
-## PART6 - Regression Analysis: "4046 and "Total Bags" in Boston region
 
 # 1. Obtain the equation of the regression line.
 
