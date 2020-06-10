4db4d893598c1ee131145639a99648702f70c005
Quarre <kaankizir@gmail.com>
2020-06-10 11:32:56 +0300

    Q1 desc, cat

--- Q1.R
+++ Q1.R
@@ -3,7 +3,7 @@
 library(moments)
 
 printf <- function(...) {
-	print(sprintf(...))
+	cat(sprintf(...), "\n")
 }
 
 fractile <- function(data_set, n) {
@@ -39,20 +39,22 @@ avocado_data <- read.csv("avocado.csv")
 atlanta <- avocado_data[avocado_data[,"region"] == "Atlanta",]
 atlanta_total <- atlanta[,"Total.Volume"]
 
+printf("The data set describes avacodo sales with prices and the amount of units in certain regions of America")
+printf("Accessed from : https://www.kaggle.com/neuromusic/avocado-prices")
 printf("Variables :")
-printf("Date         - Interval")
-printf("AveragePrice - Interval")
-printf("Total Volume - Interval")
-printf("4046         - Interval")
-printf("4225         - Interval")
-printf("4770         - Interval")
-printf("Total Bags   - Interval")
-printf("Small Bags   - Interval")
-printf("Large Bags   - Interval")
-printf("XLarge Bags  - Interval")
-printf("type         - Nominal")
-printf("year         - Interval")
-printf("region       - Nominal")
+printf("Date         - Interval - The date the entry was recorded at")
+printf("AveragePrice - Interval - The average price of an avacado in the entry")
+printf("Total Volume - Interval - The total count of avocado sold in the entry")
+printf("4046         - Interval - The count of avocados sold with code 4046 in the entry")
+printf("4225         - Interval - The count of avocados sold with code 4225 in the entry")
+printf("4770         - Interval - The count of avocados sold with code 4770 in the entry")
+printf("Total Bags   - Interval - The total bags of avocado sold in the entry")
+printf("Small Bags   - Interval - The count of small bags sold in the entry")
+printf("Large Bags   - Interval - The count of large bags sold in the entry")
+printf("XLarge Bags  - Interval - The count of extra large bags sold in the entry")
+printf("type         - Nominal  - Whether the sold avocados are organic or conventional")
+printf("year         - Interval - The year the entry was recorded at")
+printf("region       - Nominal  - The region where the data entry was located at")
 
 basic_descriptive(atlanta_total)
 plot(atlanta_total, atlanta[,"AveragePrice"])
