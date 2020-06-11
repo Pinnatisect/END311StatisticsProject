6ecc0c184dd7ddab7d26d9d86eecf6c5bcd907e0
Quarre <kaankizir@gmail.com>
2020-06-11 13:42:04 +0300

    Q4.R t.test

--- Q4.R
+++ Q4.R
@@ -20,6 +20,7 @@ var_one <- var(boston_total)
 var_two <- var(atlanta_total)
 
 v <- ((var_one / n_one + var_two / n_two)^2) / (((var_one / n_one)^2) / (n_one - 1) + ((var_two / n_two)^2) / (n_two - 1))
+print(v)
 
 t <- (-qt((1 - confidence) / 2 , df=v))
 
@@ -30,3 +31,6 @@ estimate <- mean_one - mean_two
 print("Confidence interval:")
 print(sprintf("%s - %s * %s < mu_one - mu_two < %s + %s * %s", estimate, t, std_err, estimate, t, std_err))
 print(sprintf("%s - %s < mu_one - mu_two < %s + %s", estimate, t * std_err, estimate, t * std_err))
+print(sprintf("%s < mu_one - mu_two < %s", estimate - t * std_err, estimate + t * std_err))
+
+t.test(atlanta_total, mu = 0, var.equal = FALSE, alternative = "two.sided", conf.level = confidence)
