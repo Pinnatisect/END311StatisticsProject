2090ca78d50cc9675f1173e2c6bf352872f617fa
Quarre <kaankizir@gmail.com>
2020-06-11 18:56:36 +0300

    document

--- Q3.R
+++ Q3.R
@@ -1,16 +1,16 @@
 # PART3 : Hypothesis testing for standard deviation
+
 library(data.table)
 library(TeachingDemos) #for sigma.test function
 
 my_avocado_data <- read.csv("avocado.csv", sep = ",", header = T)
 atlanta_data <- subset(my_avocado_data, region == "Atlanta")
+atlanta <- atlanta_data$Total.Volume
 
 # 1. State the hypothesis.
 # H0(Null hypothesis): standard deviation of total amount of avocado sold (in volume) in Atlanta region is equal to 200000(test value)
 # H1(alternative hypothesis): standard deviation of total amount of avocado sold (in volume)in Atlanta region is greater than 200000(test value)
 
-options(scipen = 999)
-atlanta <- atlanta_data$Total.Volume
 test_sigma <- 200000
 
 # 2. Compute the test statistic.
